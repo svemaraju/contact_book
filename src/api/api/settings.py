336c715b14382f3a706d6f67cd3d9004dccf6e72
Srikanth Vemaraju <srikanth.vemaraju@hedgehoglab.com>
2018-12-16 21:48:42 +0530

    Heroku configurations.

--- src/api/api/settings.py
+++ src/api/api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,14 +80,11 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'contactbook',
-        'USER': 'contactbook',
-        'PASSWORD': '*E:T2bj*[TQVPJBL',
-        'HOST': '127.0.0.1'
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
